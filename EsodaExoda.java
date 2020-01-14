43d47b8bcff6f54c5cd71ec720c376b7a25983c2
litsas7 <57151026+litsas7@users.noreply.github.com>
2020-01-14 16:53:14 +0200

    Add files via upload

--- EsodaExoda.java
+++ EsodaExoda.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 public class EsodaExoda {
-        public void esodaExoda() {
+        public static void main(String[] args) {
 		double esoda = 0;
 		double exoda = 0;
 		double kz;
@@ -9,7 +9,7 @@ public class EsodaExoda {
 		Scanner input = new Scanner(System.in);
 		ArrayList<Double> es = new ArrayList<Double>();
 		ArrayList<Double> ex = new ArrayList<Double>();
-		System.out.println("Eisagete esoda, exoda. Ta exoda me arnitiko prosimo. H diadikasia oloklironetai an eisagete to 0");
+		System.out.println("Input your incomes, costs. Enter your costs with negative sign. Doctrine terminates if you insert 0");
 		x = input.nextDouble();
 		while (x != 0) {
 			if (x > 0) {
@@ -25,20 +25,20 @@ public class EsodaExoda {
 		for (int b = 0; b < ex.size(); b++) {
 			exoda = exoda + ex.get(b);
 		}
-		System.out.printf("Ta exoda einai: %.2f%n", exoda);
-		System.out.printf("Ta esoda xwris forologia einai: %.2f%n", esoda);
+		System.out.printf("Total cost is: %.2f%n", exoda);
+		System.out.printf("Income without tax is: %.2f%n", esoda);
 		double foros = (24 * esoda) / 100;
 		esoda = esoda - foros;
-		System.out.printf("O foros einai: %, 2f%n", foros);
-		System.out.printf("Ta esoda meta tin forologia einai: %.2f%n", esoda);
+		System.out.printf("Tax is: %, 2f%n", foros);
+		System.out.printf("Income with tax is: %.2f%n", esoda);
 		kz = esoda - exoda;
 		if (kz > 0) {
-			System.out.printf("To kerdos einai: %.2f%n", kz);
+			System.out.printf("Profit is: %.2f%n", kz);
 		} else if (kz < 0) {
-			System.out.printf("H zimia einai: %.2f%n", kz);
+			System.out.printf("Damage is: %.2f%n", kz);
 		}
 		else
-			System.out.printf("Ta esoda einai isa me ta exoda");
+			System.out.printf("Income equals to cost");
 
 	}
 
