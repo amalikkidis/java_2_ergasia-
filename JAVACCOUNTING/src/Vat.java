2539a8c6c3d8aeb6d3a0d1a4272049d1459fcb1e
kpsill <57151023+kpsill@users.noreply.github.com>
2020-01-15 03:20:02 +0200

    Update Vat.java

--- JAVACCOUNTING/src/Vat.java
+++ JAVACCOUNTING/src/Vat.java
@@ -17,7 +17,7 @@ public enum Vat {
         return value;
     }
     
-    public String getVatString() { //metatrepei to vat apo dekadiko se string %, px to 0.23 to kanei 23% 
+    public String getVatString() { //metatrepei to vat apo dekadiko se string %, px to 0.23 to kanei "23.00%" 
     	return getValue().multiply(new BigDecimal(100)).toString() + "%";
     }
 }
